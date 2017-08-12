474b9021c0d2b2550a495c0f8f88d45c6f76491a
heboliufengjie <heboliufengjie@126.com>
2017-08-12 20:41:33 +0800

    update

---
+++ pages/login/login.js
@@ -0,0 +1,38 @@
+//login.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  //register
+  register: function () {
+    wx.navigateTo({
+      url: '../register/register'
+    })
+  },
+  //forgotPassword
+  forgotPassword: function () {
+    wx.navigateTo({
+      url: '../forgotPassword/forgotPassword'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+})
